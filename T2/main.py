da966b8a8e57cf5a4d14dfbcdf1ccf9fd09df96f
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2022-08-02 13:47:46 +0430

    finished

--- T2/main.py
+++ T2/main.py
@@ -7,15 +7,15 @@ def checkRange(age, rangeN1, rangeN2):
         return False
 
 
-def checkMail(gender):
+def checkMale(gender):
     if gender[0] == 'm':
         return True
     else:
         return False
 
 
-def checkMovie(type):
-    if type[0] == 'm':
+def checkMovie(kind):
+    if kind[0] == 'm':
         return True
     else:
         return False
@@ -23,31 +23,31 @@ def checkMovie(type):
 
 # base codes
 
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 gender = input("Enter your gender: ")
-type = input("What type do you want? ")
+kind = input("What type do you want? ")
 
-if checkRange(age, 0, 16) and checkMail(gender) and checkMovie():
+if checkRange(age, 0, 16) and checkMale(gender) and checkMovie(kind):
     print("You should see Coco")
-elif checkRange(age, 0, 16) and checkMail(gender) and not checkMovie():
+elif checkRange(age, 0, 16) and checkMale(gender) and not checkMovie(kind):
     print("You should see Stranger Things")
-elif checkRange(age, 0, 16) and not checkMail(gender) and checkMovie():
+elif checkRange(age, 0, 16) and not checkMale(gender) and checkMovie(kind):
     print("You should see Frozen")
-elif checkRange(age, 0, 16) and not checkMail(gender) and not checkMovie():
+elif checkRange(age, 0, 16) and not checkMale(gender) and not checkMovie(kind):
     print("You should see Madereseh Mushha")
-elif checkRange(age, 17, 35) and checkMail(gender) and checkMovie():
+elif checkRange(age, 17, 35) and checkMale(gender) and checkMovie(kind):
     print("You should see Fight Club")
-elif checkRange(age, 17, 15) and checkMail(gender) and not checkMovie():
+elif checkRange(age, 17, 15) and checkMale(gender) and not checkMovie(kind):
     print("You should see Peaky Blinders")
-elif checkRange(age, 17, 35) and not checkMail(gender) and checkMovie():
+elif checkRange(age, 17, 35) and not checkMale(gender) and checkMovie(kind):
     print("You should see Titanic")
-elif checkRange(age, 17, 35) and not checkMail(gender) and not checkMovie():
+elif checkRange(age, 17, 35) and not checkMale(gender) and not checkMovie(kind):
     print("You should see Friends")
-elif checkRange(age, 36, 50) and checkMail(gender) and checkMovie():
+elif checkRange(age, 36, 50) and checkMale(gender) and checkMovie(kind):
     print("You should see Its a WonderFul Life")
-elif checkRange(age, 36, 50) and checkMail(gender) and not checkMovie():
+elif checkRange(age, 36, 50) and checkMale(gender) and not checkMovie(kind):
     print("You should see Fargo")
-elif checkRange(age, 36, 50) and not checkMail(gender) and checkMovie():
+elif checkRange(age, 36, 50) and not checkMale(gender) and checkMovie(kind):
     print("You should see Gone with the wind")
-elif checkRange(age, 36, 50) and not checkMail(gender) and not checkMovie():
+elif checkRange(age, 36, 50) and not checkMale(gender) and not checkMovie(kind):
     print("You should see Sharzad")
