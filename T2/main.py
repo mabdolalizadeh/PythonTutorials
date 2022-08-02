79cb50b7c03130151ad731755f536a16776101c1
mabdolalizadeh <m.abdolalizadeh@outlook.com>
2022-08-02 13:28:52 +0430

    update

---
+++ T2/main.py
@@ -0,0 +1,17 @@
+def checkRange(age, rangeN1, rangeN2):
+    if rangeN1 <= age <= rangeN2:
+        return 1
+
+
+def checkMail(gender):
+    if gender[0] == 'm':
+        return 1
+    else:
+        return 0
+
+
+def checkMovie(type):
+    if type[0] == 'm':
+        return 1
+    else:
+        return 0
